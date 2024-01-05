811953c2a1e3e5b4ec3b36ea59caff67a3ce5c45
orianasosa <sosa.a.ori@gmail.com>
2024-01-05 18:12:28 -0300

    prueba dos

--- orianaSitioWeb/src/App.jsx
+++ orianaSitioWeb/src/App.jsx
@@ -11,6 +11,8 @@ function App() {
       <NavBar/>
       <Main/>
       <Footer/>
+      <img src="../../src/assets/guardaSitioWeb.png"/>
+      <img src="https://imgs.search.brave.com/jN-ovIhLZlqjBsdKXc19Exl66At2XLlnRwO80qvMvuw/rs:fit:500:0:0/g:ce/aHR0cHM6Ly93d3cu/ZG9taW5pY2FuY29v/a2luZy5jb20vd3At/Y29udGVudC91cGxv/YWRzL2VtcGFuc2l0/YXMtcGFzdGVsaXRv/cy1zdGVwLTI4Lmpw/Zw" alt="" />
     </>
   )
 }
