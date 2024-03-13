bee7246a706637059e84a9182a03464d518b2858
orianasosa <sosa.a.ori@gmail.com>
2024-03-13 15:16:20 -0300

    footerImg

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,6 @@
 import styles from "./Footer.css"
+import linkedin from "./../../assets/linkedin.png"
+import github from "./../../assets/github.png"
 
 const Footer = () => {
   return (
@@ -9,8 +11,8 @@ const Footer = () => {
             <p className="footerName m-2">Oriana Sosa</p>
           </div>
           <div className="col-6 d-flex justify-content-end align-items-end">
-            <a href="https://www.linkedin.com/in/oriana-sosa/" target="_blank" rel="noopener noreferrer"><img src="../../src/assets/linkedin.png" width="50px" height="50px" alt="" /></a>
-            <a href="https://github.com/Oriana-Sosa" target="_blank" rel="noopener noreferrer"><img src="../../src/assets/github.png"  width="50px" height="50px" alt=""/></a>
+            <a href="https://www.linkedin.com/in/oriana-sosa/" target="_blank" rel="noopener noreferrer"><img src={linkedin} width="50px" height="50px" alt="" /></a>
+            <a href="https://github.com/Oriana-Sosa" target="_blank" rel="noopener noreferrer"><img src={github}  width="50px" height="50px" alt=""/></a>
           </div>
         </div>
       </div>
